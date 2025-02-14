34da4e45f4256021e0680376c100f4cf03d9f6a8
rusafan <afanasyev.ruslan@gmail.com>
2025-02-14 20:24:58 +0300

    3. Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ruslan Afanasev");
+        System.out.println("31.01.2025");
+    }
+}
