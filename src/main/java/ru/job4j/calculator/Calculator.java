f36a833912d598a6e4caa5ed81ac17ff6141c807
rusafan <afanasyev.ruslan@gmail.com>
2025-02-14 21:32:04 +0300

    4.1. Разбор первой программы. [#159033 #417816]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,7 +8,6 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
-
         int onePlusTwo = one + two;
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
