4cf6e73cd9d4767b1368e4985ca32e5b91c62037
rusafan <afanasyev.ruslan@gmail.com>
2025-02-14 21:27:25 +0300

    4.1. Разбор первой программы. [#159033 #417816]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Ruslan Afanasev");
-        System.out.println("31.01.2025");
+        System.out.println("14.02.2025");
     }
 }
