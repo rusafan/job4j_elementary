9e75ef5e4cfb568bcefa4f8f613df743313ef063
rusafan <afanasyev.ruslan@gmail.com>
2025-02-26 22:55:02 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905] 0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,10 +13,15 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(240);
-        System.out.println("240 rubles are " + dollar + " dollar.");
-
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        input = 240;
+        expected = 4;
+        output = Converter.rubleToDollar(input);
+        passed = expected == output;
+        System.out.println("240 rubles are 4. Test result : " + passed);
     }
 }
