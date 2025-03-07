bd8bb294761ae07c3df0560c911757dc81b3cd68
rusafan <afanasyev.ruslan@gmail.com>
2025-03-08 01:28:33 +0300

    0. Операции сравнения [#235631 #535212]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Liken {
     public static void main(String[] args) {
-        int first = 10;
+        int first = 11;
         int second = 9;
 
         boolean result = first > second;
